e052a262793e8de56d8675c5977300c5110daa6b
Moi-29 <moibonberhanu6@gmail.com>
2025-12-02 23:59:03 +0900

    Initial commit

---
+++ utils.js
@@ -0,0 +1,11 @@
+function generateRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+function celsiusToFarenheit(celsius) {
+  return (celsiue * 9) / 5 + 32;
+}
+module.exports = {
+  generateRandomNumber,
+  celsiusToFarenheit,
+};
