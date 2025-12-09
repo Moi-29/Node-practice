50c4502cf35f6a4f5c084a12ee0c8e7b49c690af
Moi-29 <moibonberhanu6@gmail.com>
2025-12-09 13:51:05 +0900

    changes

--- file_read.js
+++ file_read.js
@@ -1,8 +1,12 @@
 import fs from "fs";
 
-fs.readFile("./doc.txt", (err, data) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(data.toString());
+// fs.readFile("./doc.txt", (err, data) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(data.toString());
+// });
+
+fs.writeFile("./doc.txt", "Hello, SWE", () => {
+  console.log("Your file is overwritten!");
 });
