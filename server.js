e4c0c9ae411d869584d904baee05cb3baeb1b64d
Moi-29 <moibonberhanu6@gmail.com>
2025-12-12 21:46:45 +0900

    changes

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+import http from "http";
 
+const server = http.createServer((req, res) => {
+  console.log("request made");
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("Server is on http://localhost:3000");
+});
