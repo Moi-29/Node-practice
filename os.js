129d171b6606fc56bdc45c5b3edd3f76fd987a25
Moi-29 <moibonberhanu6@gmail.com>
2025-12-16 15:04:58 +0300

    OS information...

---
+++ os.js
@@ -0,0 +1,6 @@
+import os from "os";
+
+console.log("Platform:", os.platform());
+console.log("CPU Arch:", os.arch());
+console.log("Total Memory:", os.totalmem());
+console.log("Free Memory:", os.freemem());
