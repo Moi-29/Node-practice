5a92b4956a3381c69cb87562968e71e01aa62ec3
Moi-29 <moibonberhanu6@gmail.com>
2025-12-16 14:44:31 +0300

    reading files

--- file_read.js
+++ file_read.js
@@ -1,12 +1,8 @@
 import fs from "fs";
 
-// fs.readFile("./doc.txt", (err, data) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(data.toString());
-// });
-
-fs.writeFile("./doc.txt", "Hello, SWE", () => {
-  console.log("Your file is overwritten!");
+fs.readFile("doc.txt", (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(data.toString);
 });
