f405c9f6d8170a30f6cf1e43c9ba9d1b912dd43e
Moi-29 <moibonberhanu6@gmail.com>
2025-12-17 16:30:39 +0300

    Request, Response handling...

--- serv.js
+++ serv.js
@@ -1,6 +1,14 @@
 import http from "http";
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Welcome to the Home Page");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Page Not Found");
+  }
+});
 
 server.listen(3000, () => {
   console.log("server is runnunig on port 3000");
