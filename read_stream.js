ea0502eff4736e155b71567a7bac0a5580a7a8d7
Moi-29 <moibonberhanu6@gmail.com>
2025-12-17 09:34:25 +0300

    Stream, chunk...

---
+++ read_stream.js
@@ -0,0 +1,15 @@
+import fs from "fs";
+
+const readableStream = fs.createReadStream("doc.txt", { encoding: "utf-8" });
+
+readableStream.on("data", (chunk) => {
+  console.log(chunk);
+});
+
+readableStream.on("end", () => {
+  console.log("Finished reading the file.");
+});
+
+readableStream.on("error", (err) => {
+  console.err("Error:", err);
+});
