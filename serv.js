876aef2c541777f5044e2359612dfc84dd87f5d3
Moi-29 <moibonberhanu6@gmail.com>
2025-12-17 17:26:19 +0300

    Query handling...

--- serv.js
+++ serv.js
@@ -1,4 +1,5 @@
 import http from "http";
+import url from "url";
 
 // const server = http.createServer((req, res) => {
 //   if (req.method === "GET" && req.url === "/") {
@@ -14,26 +15,43 @@ import http from "http";
 //   console.log("server is runnunig on port 3000");
 // });
 
+// const server = http.createServer((req, res) => {
+//   if (req.method === "POST" && req.url === "./data") {
+//     let bodey = "";
+
+//     req.on("data", (chunk) => {
+//       body += chunk.toString();
+//     });
+
+//     req.on("end", () => {
+//       res.writeHead(200, { "content-type": "application/json" });
+//       res.end(
+//         JSON.stringify({ message: "Received successfully", data: "body" })
+//       );
+//     });
+//   } else {
+//     res.writeHead(404, { "content-type": "text/plain" });
+//     res.end("404 Route Not Found");
+//   }
+// });
+
+// server.listen(3000, ()=>{
+// 	console.log('The site is running on port 3000');
+
+// })
+
 const server = http.createServer((req, res) => {
-  if (req.method === "POST" && req.url === "./data") {
-    let bodey = "";
+  if (req.method === "GET" && req.url === "/search") {
+    const queryObject = url.parse(req.url, true).query;
 
-    req.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-
-    req.on("end", () => {
-      res.writeHead(200, { "content-type": "application/json" });
-      res.end(
-        JSON.stringify({ message: "Received successfully", data: "body" })
-      );
-    });
+    res.writeHead(200, { "content-type": "application/json" });
+    res.end(JSON.stringify({ message: "Received", queryObject }));
   } else {
     res.writeHead(404, { "content-type": "text/plain" });
-    res.end("404 Route Not Found");
+    res.end("Not Found");
   }
 });
 
 server.listen(3000, () => {
-  console.log("The site is running on port 3000");
+  console.log("Server is running under port 3000");
 });
