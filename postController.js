191b704e1e4ac5c905ed7cf04fa35aeef5570e53
Moi-29 <moibonberhanu6@gmail.com>
2025-12-22 16:06:10 +0300

    modification...

--- postController.js
+++ postController.js
@@ -1,6 +1,6 @@
 const posts = [
-  { id: 1, title: "post one" },
-  { id: 2, title: "post two" },
+  { id: 1, title: "First Post" },
+  { id: 2, title: "Second Post" },
 ];
 
 const getPosts = () => posts;
