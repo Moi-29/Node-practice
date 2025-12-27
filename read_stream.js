704a7f490b2ad4f6fcbc6e7ddc756a1253f5cc7b
Moi-29 <moibonberhanu6@gmail.com>
2025-12-27 09:20:54 +0300

    modifications...

--- read_stream.js
+++ read_stream.js
@@ -7,7 +7,7 @@ readableStream.on("data", (chunk) => {
 });
 
 readableStream.on("end", () => {
-  console.log("The file is read...");
+  console.log("Finished reading the file...");
 });
 
 readableStream.on("error", (err) => {
